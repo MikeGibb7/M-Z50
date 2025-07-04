7f8940b4635d2a031a085e3fdbc25a6d1eac651e
MikeGibb7 <119349139+MikeGibb7@users.noreply.github.com>
2025-07-04 12:11:28 -0400

    add ui component

--- sp_tickers.py
+++ sp_tickers.py
@@ -16,3 +16,17 @@ def get_sp500_companies():
     changes_df = sp_table[1]
     
     return stock_df, changes_df
+def get_sp500_companies_windustry():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    sp_table = pd.read_html(url)
+    sp500_df = sp_table[0]
+
+    stock_df = []
+    for _, stock in sp500_df.iterrows():
+        symbol = stock["Symbol"].replace('.', '-')  # optional fix for e.g., BRK.B
+        industry = stock["GICS Sub-Industry"]
+        stock_df.append((symbol, industry))
+        
+    changes_df = sp_table[1]
+    
+    return stock_df, changes_df
